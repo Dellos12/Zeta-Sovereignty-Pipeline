15af7169e78e84ac22b51a919a3a4cda37261603
Dellos12 <DellosDevOps@outlook.com>
2026-03-31 03:05:17 -0300

    fix: alterando host para url no initializer do redis

--- Manifold/config/initializers/redis.rb
+++ Manifold/config/initializers/redis.rb
@@ -1,21 +1,16 @@
 require 'redis'
 
-# Pega a URL completa do Jenkins ou usa localhost como fallback
-redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')
+# Pegamos a URL que o Jenkins injetou
+redis_url = ENV.fetch('REDIS_URL', 'redis://redis:6379/1')
 
-# CORREÇÃO CRUCIAL: Use 'url:' para que o Ruby entenda o protocolo redis://
-$redis = Redis.new(url: redis_url)
+# AQUI ESTÁ O SEGREDO: Use "url:" e NÃO "host:"
+# O "url:" faz o Ruby entender o protocolo redis://
+$redis = Redis.new(url: redis_url) 
 
 begin
-  retries ||= 0
   $redis.ping
   puts "📡 [REDIS] Conexão Estabelecida: Hiperplano Ativo."
 rescue StandardError => e
-  if (retries += 1) < 3
-    sleep 1
-    retry
-  end
-  # Agora o erro mostrará o que realmente falhou
-  puts "⚠️ [REDIS] Offline: Verificando infra em #{redis_url}. Erro: #{e.message}"
+  puts "⚠️ [REDIS] Offline: Falha ao conectar em #{redis_url}. Erro: #{e.message}"
 end
 
